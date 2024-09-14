5120991b4f57e006b3fd80f698e3a577bbb43e83
neok02 <vlad15082006@gmail.com>
2024-09-14 12:09:17 +0400

    Add files via upload

---
+++ spisok/neok/neok/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                pictureBox2.Hide();
+                pictureBox1.Show();
+            }
+            else
+            {
+                pictureBox1.Hide();
+                pictureBox2.Show();
+
+            }
+        }
+    }
+}
